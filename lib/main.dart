adee988d5c73101f693bdb4ca7fb388521735cde
Dina Ahmed <denaahmed340@gmail.com>
2024-09-25 19:10:53 +0300

    core/theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'core/utils/app_theme.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,10 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tidal',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: AppTheme.lightTheme,
       home: Container()
     );
   }
