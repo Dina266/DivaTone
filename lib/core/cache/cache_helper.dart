a9bfd01e4a73997019c0e433c318961d04cbda9b
Dina Ahmed <denaahmed340@gmail.com>
2024-09-25 18:29:33 +0300

    add caches

---
+++ lib/core/cache/cache_helper.dart
@@ -0,0 +1,34 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'cache_keys.dart';
+
+class CacheHelper {
+  late SharedPreferences sharedPreferences;
+  Future<void> init() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  Future<void> saveData({required String key, required bool value}) async {
+    await sharedPreferences.setBool(key, value);
+  }
+
+  bool getData({required String key}) {
+    return sharedPreferences.getBool(key) ?? false;
+  }
+
+  Future<void> setCurrentLanguageCode({required String languageCode}) async {
+    sharedPreferences.setString(CacheKeys.currentLanguageCode, languageCode);
+  }
+
+  Future<void> setCurrentLanguage({required String language}) async {
+    sharedPreferences.setString(CacheKeys.currentLanguage, language);
+  }
+
+  String? getCurrentLanguageCode() {
+    return sharedPreferences.getString(CacheKeys.currentLanguageCode);
+  }
+
+  String? getCurrentLanguage() {
+    return sharedPreferences.getString(CacheKeys.currentLanguage);
+  }
+
+}
---
+++ lib/core/cache/cache_keys.dart
@@ -0,0 +1,5 @@
+abstract class CacheKeys {
+  static const String onboardingKey = 'onboarding';
+  static const String currentLanguageCode = 'currentLanguageCode';
+  static const String currentLanguage = 'currentLanguage';
+}
